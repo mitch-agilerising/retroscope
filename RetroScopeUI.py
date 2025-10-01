0861441e4a5f17e3c7c986273272d10dd21cedb9
Mitch Goldstein <mitch.goldstein@agilerising.com>
2025-09-30 21:45:27 -0400

    First foray

---
+++ RetroScopeUI.py
@@ -0,0 +1,8 @@
+from PySide6 import QtCore, QtWidgets
+
+
+class RetroScopeUI(QtWidgets.QWidget):
+    def __init__(self):
+        super().__init__()
+
+
---
+++ main.py
@@ -0,0 +1,15 @@
+"""
+Here's mud in yer eye
+"""
+import sys
+from PySide6 import QtWidgets, QtGui
+
+from RetroScopeUI import RetroScopeUI
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+    widget = RetroScopeUI()
+    widget.resize(800, 600)
+    widget.show()
+
+    sys.exit(app.exec())
